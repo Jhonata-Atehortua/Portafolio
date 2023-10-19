a665df503e98a18374b8f1769135bac70349d279
Jhonatan Atehortua <jhonatanprogramacion0@gmail.com>
2023-10-18 20:01:35 -0500

    SUbir

--- app/app/settings.py
+++ app/app/settings.py
@@ -65,9 +65,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # ...
 
+
+
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('JAWSDB_MARIA_URL'))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
